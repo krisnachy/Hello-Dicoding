1a35f551a96a8b8f8d5526d8c0d257c93063b67a
krisnachy <cahyayuana@gmail.com>
2023-06-05 15:01:20 +0700

    Initial Commit

---
+++ Hello Dicoding/Hello_DicodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_DicodingApp.swift
+//  Hello Dicoding
+//
+//  Created by Cahyo on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_DicodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
